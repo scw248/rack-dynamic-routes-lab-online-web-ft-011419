57ea3f758074a98be7f342928317517bdfd8eeca
Stephen Wright <scw248@gmail.com>
2019-02-21 01:23:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       
       item_name = req.path.split("/items/").last
-      item = @@items.find{|s| s.name == item_name}
+      item = @@item.find{|s| s.name == item_name}
       if item
         item.price
       else
